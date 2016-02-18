8c1f1146a97c3149b0b1568e6fae235879ee7a40
takuya0987 <tark0789@icloud.com>
2016-02-18 14:33:54 +0900

    Initial Commit

---
+++ Uranai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Uranai
+//
+//  Created by moritakuya on 2016/02/18.
+//  Copyright © 2016年 moritakuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
